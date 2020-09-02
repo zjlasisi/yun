fe781068d600fb6709511c27b0f2a73ec9a578f1
陈康钟 <15967160657@163.com>
2020-09-02 16:58:48 +0800

    -- user=ckz mybatis-plus

--- src/main/java/com/technology/yun/YunApplication.java
+++ src/main/java/com/technology/yun/YunApplication.java
@@ -1,9 +1,11 @@
 package com.technology.yun;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.technology.yun.db.dao")
 public class YunApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/technology/yun/db/model/SysLog.java
+++ src/main/java/com/technology/yun/db/model/SysLog.java
@@ -3,8 +3,12 @@ package com.technology.yun.db.model;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+import java.util.Date;
+
 @Data
-@Document(collection = "yun")
-public class SysLog {
+@Document
+public class SysLog implements Serializable {
     private String log;
+    private Date createdAt;
 }
